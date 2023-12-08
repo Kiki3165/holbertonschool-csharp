6848aaeba8a83c30e461f87e812f1e766630aaf3
Kiki3165 <kikikou82@gmail.com>
2023-12-08 13:15:06 +0000

    task6

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		string repeat = str.Repeat(3);
+        Console.WriteLine(repeat);
+        }
+}
