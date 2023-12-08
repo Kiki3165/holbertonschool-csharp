d570d989d5bbf0acbb4ef9d0c9bccd41bef4d684
Kiki3165 <113670617+Kiki3165@users.noreply.github.com>
2023-12-08 16:32:50 +0100

    task6

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,8 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		string var = str.Substring(0,9);
         Console.WriteLine("{0}, {1}, {2}", str);
-		Console.WriteLine(var);
+		Console.WriteLine(str.Substring(0,9));
         }
 }
