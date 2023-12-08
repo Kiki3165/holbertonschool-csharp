f485b7259bfdc4a918b2d47be1eaed4ddd0a46d8
Kiki3165 <kikikou82@gmail.com>
2023-12-08 12:28:19 +0000

    test3

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,8 +1,14 @@
+using system;
+
+namespace 3-WriteLine
+{
 class Sample
+
     {
-        static void Main()
+        static void Main(string[] args)
         {
             string a = "\"Programming is like building a multilingual puzzle\n";
             Console.WriteLine(a);
         }
     }
+}
