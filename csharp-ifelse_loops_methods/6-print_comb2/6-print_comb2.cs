df0cedfc55384ce7be2a89149726f012580f06ba
Kiki3165 <113670617+Kiki3165@users.noreply.github.com>
2023-12-09 19:31:43 +0100

    task6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i <= 9; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                Console.Write($"{i:D2}, {j:D2}");
+                if (i != 8 || j != 9)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+}
