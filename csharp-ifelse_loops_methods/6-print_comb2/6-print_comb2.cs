8aac4890e5909bb9fe5ea153e720be5b368bda60
Kiki3165 <113670617+Kiki3165@users.noreply.github.com>
2023-12-09 20:14:05 +0100

    change writeline

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -8,13 +8,13 @@ class Program
         {
             for (int j = i + 1; j <= 9; j++)
             {
-                Console.Write($"{i:D2}, {j:D2}");
+                Console.Write($"{i:D1}{j:D1}");
                 if (i != 8 || j != 9)
                 {
                     Console.Write(", ");
                 }
             }
         }
+
         Console.WriteLine();
-    }
 }
