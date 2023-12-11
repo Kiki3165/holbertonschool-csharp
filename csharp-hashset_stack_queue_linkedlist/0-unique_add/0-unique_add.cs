73e485cff69357c82da7641f8a98f02bdcb17994
Kiki3165 <113670617+Kiki3165@users.noreply.github.com>
2023-12-11 11:00:20 +0100

    task0

--- csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -1,7 +1,10 @@
 using System;
-
-public static int Sum(List<int> myList)
-{
-    var set = new HashSet<int>{myList};
-    Console.WriteLine(string.Join(",", unique));
-}
+using System.Collections.Generic;
+    class Program
+    {
+        public static int Sum(List<int> myList)
+        {
+            var set = new HashSet<int>{myList};
+            Console.WriteLine(string.Join(",", unique));
+        }
+    }
