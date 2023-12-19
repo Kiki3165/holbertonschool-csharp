a18fd10820371f61234b352933382d183665adca
Kiki3165 <113670617+Kiki3165@users.noreply.github.com>
2023-12-19 18:22:41 +0100

    task1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> item = list1.FindAll(elem => list2.Contains(elem));
+        return (item);
+    }
+}
